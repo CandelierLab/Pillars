2f104fb2cb02f9dcc293bcdab6883963f119ebfe
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2022-03-17 11:00:29 +0100

    Add Export options

--- +GUI/@Filters/setOption.m
+++ +GUI/@Filters/setOption.m
@@ -22,6 +22,24 @@ switch name
 
         this.disp.marg = this.elms.pdf_marg.Value || this.elms.pdf_both.Value;
         this.disp.filt = this.elms.pdf_filt.Value || this.elms.pdf_both.Value;
-        
         this.Update
+
+    case 'export2WS'
+
+        assignin('base', 'Events', this.E(this.sub));
+
+        fprintf('------------------------------------\n');
+        fprintf('Successfully imported %i events in the ''Events'' variable of the workspace.\n', numel(this.sub));
+
+    case 'export2file'
+
+        fname = uiputfile('*.mat');
+        if fname
+            Events = this.E(this.sub);
+            save(fname, 'Events');
+
+            fprintf('------------------------------------\n');
+            fprintf('Successfully saved %i events in ''%s''.\n', numel(this.sub), fname);
+
+        end
 end
